41d0701b611a0d8cbe6d4752b7f138fbee546328
Leonardo Pérez-Castilla <128614607+lpcastilla@users.noreply.github.com>
2023-09-14 16:18:01 +0000

    se creo el modelo de IG

--- src/models.py
+++ src/models.py
@@ -7,24 +7,40 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(500), nullable=False)
+    password = Column(String(500), nullable=False)
+    followers = relationship('Followers', back_populates='user')
+    post = relationship('Post', back_populates='user')
+    comment = relationship('Comment', back_populates='user')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Followers(Base):
+    __tablename__ = 'followers'
+    user_from_id = Column(Integer, nullable=False)
+    id_user = Column(Integer, ForeignKey('user.id'), nullable=False, primary_key=True)
+    user = relationship("User", back_populates='followers')
+
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id_user = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship("User", back_populates='post')
+    comment = relationship("Comment", back_populates='post')
 
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(2000), nullable=False)
+    id_user = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship("User", back_populates='comment')
+    id_post = Column(Integer, ForeignKey('post.id'), nullable=False)
+    post = relationship("Post", back_populates='comment')
+    
+ 
     def to_dict(self):
         return {}
 
@@ -35,3 +51,6 @@ try:
 except Exception as e:
     print("There was a problem genering the diagram")
     raise e
+
+ ##  person_id = Column(Integer, ForeignKey('person.id'))
+ ##   person = relationship(Person)
